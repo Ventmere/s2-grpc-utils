f95514b2e84813863e0476da39266ad5c98d78cb
Flux Xu <fluxxu@gmail.com>
2020-08-01 11:38:40 -0400

    fix: Improve error message.

--- crates/s2-grpc-utils-derive/src/lib.rs
+++ crates/s2-grpc-utils-derive/src/lib.rs
@@ -7,25 +7,34 @@ use syn::{parse_macro_input, DeriveInput};
 mod derive_s2_proto;
 mod derive_s2_proto_enum;
 
+macro_rules! try_parse {
+  ($e:expr) => {
+    match $e {
+      Ok(v) => v,
+      Err(e) => return TokenStream::from(e.write_errors()),
+    }
+  };
+}
+
 #[proc_macro_derive(S2ProtoPack, attributes(s2_grpc))]
 pub fn derive_pack(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
   let input = parse_macro_input!(input as DeriveInput);
-  let receiver = derive_s2_proto::InputReceiver::from_derive_input(&input).unwrap();
+  let receiver = try_parse!(derive_s2_proto::InputReceiver::from_derive_input(&input));
   TokenStream::from(quote!(#receiver))
 }
 
 #[proc_macro_derive(S2ProtoUnpack, attributes(s2_grpc))]
 pub fn derive_unpack(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
   let input = parse_macro_input!(input as DeriveInput);
-  let receiver = derive_s2_proto::InputReceiver::from_derive_input(&input)
-    .unwrap()
-    .to_unpack();
+  let receiver = try_parse!(derive_s2_proto::InputReceiver::from_derive_input(&input)).to_unpack();
   TokenStream::from(quote!(#receiver))
 }
 
 #[proc_macro_derive(S2ProtoEnum, attributes(s2_grpc))]
 pub fn derive_enum(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
   let input = parse_macro_input!(input as DeriveInput);
-  let receiver = derive_s2_proto_enum::InputReceiver::from_derive_input(&input).unwrap();
+  let receiver = try_parse!(derive_s2_proto_enum::InputReceiver::from_derive_input(
+    &input
+  ));
   TokenStream::from(quote!(#receiver))
 }
